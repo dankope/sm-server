a8162e726cf6831449685e6b8d664905cc0f0f5b
dank <git@dank.anonaddy.com>
2021-01-21 16:44:50 -0800

    more changes

--- main.go
+++ main.go
@@ -80,14 +80,6 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
 	http.ServeFile(w, r, "public/index.html")
 }
 
